7ed50946edb7d35570a6725147c1f474b0f63161
Ayush Gupta <gupta.ay@husky.neu.edu>
2018-04-13 19:46:49 -0400

    added facebook and google login api

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,8 +7,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import {Routes, RouterModule} from '@angular/router';
-import {DestinationsService} from './destinations.service';
 import { Component } from '@angular/core/src/metadata/directives';
+import {SocialLoginModule,AuthServiceConfig,FacebookLoginProvider,GoogleLoginProvider} from "angular5-social-login";
+
+
+
 // app routes will hold all the routes and the array will be of javascript object.
 const appRoutes: Routes = [
   {
@@ -24,6 +27,22 @@ const appRoutes: Routes = [
 
 ];
 
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig(
+      [
+        {
+          id: FacebookLoginProvider.PROVIDER_ID,
+          provider: new FacebookLoginProvider("1726553447414314")
+        },
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider("353435284490-u6e340tln57tvjet0vnuo3i2749pe0je.apps.googleusercontent.com")
+        }
+      ]
+  );
+  return config;
+}
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,13 +51,16 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    SocialLoginModule,
     RouterModule.forRoot(appRoutes), // to register routes in angular
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot()
   ],
-  // service file will be the provider.
-  providers: [DestinationsService],
+  providers: [ {
+    provide: AuthServiceConfig,
+    useFactory: getAuthServiceConfigs
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
