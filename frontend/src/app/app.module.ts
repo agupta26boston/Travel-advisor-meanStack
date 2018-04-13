538092c5103fb04d47e4e0014284d740df4218eb
Neha Varshney <varshney.n@husky.neu.edu>
2018-04-13 15:15:20 -0400

    added user login UI
    User Login and few files for destination service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,26 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//import { Schema } from 'mongoose';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import {Routes, RouterModule} from '@angular/router';
+import {DestinationsService} from './destinations.service';
+import { Component } from '@angular/core/src/metadata/directives';
+// app routes will hold all the routes and the array will be of javascript object.
+const appRoutes: Routes = [
+  {
+    // will create a URL: localhost:4200
+    path: '',
+    component: HomeComponent
+  } ,
+  {
+    // will create a URL: localhost:4200/login
+    path: 'login',
+    component: UserComponent
+  }
 
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes), // to register routes in angular
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot()
   ],
-  
-  providers: [],
+  // service file will be the provider.
+  providers: [DestinationsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ frontend/src/app/destinations.ts
@@ -0,0 +1,8 @@
+
+export class Destination {
+
+  _id: string;
+  title: string;
+  desc: string;
+
+}
