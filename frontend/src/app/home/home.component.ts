507d302e762f4b4df4963eca2dbb78ed01bdcf8c
janhavi <janhavi.u@husky.neu.edu>
2018-04-15 04:31:56 -0400

    Changes done to make the searched dest a clickable link

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -8,9 +8,12 @@ import { Destination } from '../destination';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  //array of destinations
   destinations: Array<Destination>;
+  //var for searched destination
   searchedDest :Destination;
+  //boolean var to condtionally display single destination or collection of destinations
+  isSingleDestination: boolean = false;
 
   constructor(private _destinationService: DestinationService) { }
 
@@ -19,13 +22,13 @@ export class HomeComponent implements OnInit {
       .subscribe(res => this.destinations = res);
   }
 
+  //on click event handler of search button on home page to return searched destination based on title
   searchDestination(title: string): Destination {
-    console.log("title is "+ title);
-   this.searchedDest= this.destinations
-    .filter(searchedDest => searchedDest.title === title)
-    .pop();
-    console.log(this.searchedDest.title);
+    this.isSingleDestination = true;
+    this.searchedDest = this.destinations
+      .filter(searchedDest => searchedDest.title === title)
+      .pop();
     return this.searchedDest;
-}
+  }
 
 }
