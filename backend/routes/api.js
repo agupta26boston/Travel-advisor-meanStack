2cffb1e04ced8478dd5098b7f15ac706ae3f5e00
Neha Varshney <varshney.n@husky.neu.edu>
2018-04-17 20:23:06 -0400

    added

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -18,50 +18,62 @@ var boston = new destination({
     desc: "Welcome to Boston!"
 });
 
-boston.save(function(err) {
-    if (err) return handleError(err);
-});
+// boston.save(function(err) {
+//     if (err) return handleError(err);
+// });
 
 var attraction1 = new attractions({
     destination_id: boston._id,
-    att_name: 'MFA',
-    att_desc: 'near NEU'
+    att_name: 'Boston harbor',
+    att_desc: 'southend'
 });
+// var attraction2 = new attractions({
+//     destination_id: boston._id,
+//     att_name: 'MFA-2',
+//     att_desc: 'near NEU-2'
+// });
 
 attraction1.save(function(err) {
     if (err) return handleError(err);
 });
+// attraction2.save(function(err) {
+//     if (err) return handleError(err);
+// });
+boston.attractionList.push(attraction1)
+    // boston.attractionList.push(attraction2)
+    //boston.attractions.push(attraction1.att_name)
 
+destination.
+findOne({ title: 'Boston' }).
+populate('attractionList'). // only works if we pushed refs to children
+exec(function(err, boston) {
+    if (err) return handleError(err);
+    console.log(boston);
+});
 
-boston.attractions.push(attraction1)
 boston.save(function(err) {
     if (err) return handleError(err);
 });
 
-attractions.
-find({ destination_id: destination._id }).
-exec(function(err, attractions) {
-    if (err) return handleError(err);
-    console.log('The attractions are an array: ', attractions);
-});
-
-
-
-
-
-
+// attractions.
+// find({ destination_id: destination._id }).
+// exec(function(err, attractionList) {
+//     if (err) return handleError(err);
+//     console.log('The attractions are an array: ', attractionList);
+// });
 
 
 
 
 
 router.get('/all', function(req, res) {
+    console.log('Getting the destinations');
     destination.find({})
         .exec(function(err, destinations) {
             if (err) {
                 console.log('Error getting the destinations');
             } else {
-                console.log(destinations);
+                // console.log(destinations);
                 res.json(destinations);
             }
         });
@@ -75,16 +87,9 @@ router.get('/destinations/:id', function(req, res) {
                 console.log('Error getting the destination');
             } else {
                 res.json(destination);
-                console.log(destination);
+                //console.log(destination);
             }
         });
 });
 
 module.exports = router;
-
-// userNew.create({
-//     name: 'user1',
-//     gender: 'female'
-// }).then(function(err, usernew) {
-//     console.log(err, usernew);
-// });0
