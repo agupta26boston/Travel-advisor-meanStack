8940a074bded83ba89552c1c879c2a3d634faa26
janhavi <janhavi.u@husky.neu.edu>
2018-04-24 22:35:26 -0400

    Added styling in home component and changes attraction component html page

--- backend/models/attraction.js
+++ backend/models/attraction.js
@@ -6,6 +6,7 @@ const attractionSchema = new Schema({
     destination_id: { type: Schema.Types.ObjectId, ref: 'destinations' },
     att_name: String,
     att_desc: String,
+    img_src:String,
     comments: Array
 });
 
--- frontend/src/app/attraction.ts
+++ frontend/src/app/attraction.ts
@@ -2,5 +2,6 @@ export class Attraction {
     _id: string;
     att_name: string;
     att_desc: string;
+    img_src:string;
     comments: Array<string>;
 }
